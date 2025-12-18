289bf653589421f48ca55a33cb88a8d2c86dcee6
shubham-kumar145 <shubhamkr5923@gmail.com>
2025-12-19 00:44:18 +0530

    Update axiosClients.js

--- frontend/src/utils/axiosClients.js
+++ frontend/src/utils/axiosClients.js
@@ -1,13 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 
-const axiosClient =  axios.create({
-    baseURL: 'http://localhost:5000',
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json'
-    }
+const axiosClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-
 export default axiosClient;
-

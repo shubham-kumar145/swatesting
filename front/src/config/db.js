b4aea08cf69358f8d0701d0f5b4b2061209fd5ab
shubham-kumar145 <shubhamkr5923@gmail.com>
2025-12-19 08:19:51 +0530

    Log MongoDB URI before connection
    Added a console log to display the MongoDB URI before connecting.

--- front/src/config/db.js
+++ front/src/config/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 
 async function main() {
+    console.log(process.env.MONGODB_URI);
     await mongoose.connect(process.env.MONGODB_URI);
 }
 
 
 module.exports = main
+

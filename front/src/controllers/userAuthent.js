44ffde80021edab5cfd7cd593636262a9a4aa619
shubham-kumar145 <shubhamkr5923@gmail.com>
2025-12-19 09:02:01 +0530

    Refactor user authentication logic and cookie settings
    Updated user authentication controller with improved cookie handling and error responses.

--- front/src/controllers/userAuthent.js
+++ front/src/controllers/userAuthent.js
@@ -1,7 +1,147 @@
+// // const redisClient = require("../config/redis")
+// // const User = require("../model/user")
+// // const validate = require("../utlis/valitator")
+// // const bcrypt = require("bcrypt")
+// // const jwt = require("jsonwebtoken")
+
+// // const register = async (req, res) => {
+// //     try {
+// //         validate(req.body)
+// //         const { firstName, emailId, password, mobileNo } = req.body;
+// //         req.body.password = await bcrypt.hash(password, 10)
+// //         req.body.role = 'user'
+
+// //         const user = await User.create(req.body)
+
+// //         const reply = {
+// //             firstName: user.firstName,
+// //             emailId: user.emailId,
+// //             _id: user._id,
+// //             role: user.role,
+// //         }
+
+// //         const token = jwt.sign({ _id: user._id, emailId: emailId, role: "user" }, "wsac", { expiresIn: 60 * 60 })
+// //         res.cookie('token', token, { maxAge: 60 * 60 * 1000 })
+
+
+// //         res.status(201).json({
+// //             user: reply,
+// //             message: 'user created successfully'
+// //         })
+
+
+// //     } catch (err) {
+// //         res.status(400).send('ERROR: ' + err)
+// //     }
+// // }
+
+// // const login = async (req, res) => {
+// //     try {
+// //         const { emailId, password } = req.body
+// //         if (!emailId) {
+// //             throw new Error("Invalid Credentials")
+// //         }
+// //         if (!password) {
+// //             throw new Error("Invalid Credentials")
+// //         }
+// //         const user = await User.findOne({ emailId })
+
+// //         if (!user) {
+// //             return res.status(401).send('Invalid Credentials');
+// //         }
+
+// //         const match = await bcrypt.compare(password, user.password)
+
+// //         if (!match) {
+// //             throw new Error('Invalid Credentials')
+// //         }
+
+// //         const reply = {
+// //             firstName: user.firstName,
+// //             emailId: user.emailId,
+// //             _id: user._id,
+// //             role: user.role
+// //         }
+
+// //         const token = jwt.sign({ _id: user._id, emailId: emailId, role: user.role }, "wsac", { expiresIn: 60 * 60 })
+// //         res.cookie('token', token, { maxAge: 60 * 60 * 1000 })
+
+// //         res.status(201).json({
+// //             user: reply,
+// //             message: "Logged In Successfully"
+// //         })
+// //     } catch (err) {
+// //         res.status(401).send('ERROR: ' + err)
+// //     }
+// // }
+
+// // const logout = async (req, res) => {
+// //     try {
+// //         const { token } = req.cookies
+// //         const payload = jwt.decode(token);
+// //         await redisClient.set(`token:${token}`, "Blocked")
+// //         await redisClient.expireAt(`token:${token}`, payload.exp)
+// //         res.cookie("token", null, { expires: new Date(Date.now()) })
+// //         res.status(200).send("Logged Out Succesfully")
+// //     } catch (err) {
+// //         res.status(503).send("ERROR: " + err)
+// //     }
+// // }
+
+// // const adminRegister = async (req, res) => {
+// //     try {
+// //         validate(req.body)
+// //         const { firstName, emailId, password, mobileNo, role } = req.body;
+// //         req.body.password = await bcrypt.hash(password, 10)
+// //         const user = await User.create(req.body)
+// //         const reply = {
+// //             firstName: user.firstName,
+// //             emailId: user.emailId,
+// //             _id: user._id,
+// //             role: user.role,
+// //         }
+// //         const token = jwt.sign({ _id: user._id, emailId: emailId, role: role }, "wsac", { expiresIn: 60 * 60 })
+// //         res.cookie('token', token, { maxAge: 60 * 60 * 1000 })
+        
+// //         res.status(201).json({
+// //             user: reply,
+// //             message: 'user created successfully'
+// //         })
+// //     } catch (err) {
+// //         res.status(400).send('ERROR: ' + err)
+// //     }
+// // }
+
+// // const deleteprofile = async (req, res) => {
+// //     try {
+// //         const userId = req.result._id;
+// //         await User.findByIdAndDelete(userId)
+// //         res.status(200).send("Deleted Successfully")
+// //     } catch (err) {
+
+// //         res.status(500).send("SERVER ERROR")
+// //     }
+// // }
+
+// // const getAllMember = async (req, res) => {
+// //     console.log("admin is here");
+
+// //     try {
+// //         const getuser = await User.find({}).select('_id firstName emailId mobileNo role purchage cart')
+// //         res.status(200).send(getuser);
+// //     } catch (err) {
+
+// //         res.status(500).send("SERVER ERROR")
+// //     }
+// // }
+
+// // module.exports = { register, login, logout, adminRegister, deleteprofile, getAllMember }
 // const redisClient = require("../config/redis")
 // const User = require("../model/user")
 // const validate = require("../utlis/valitator")
-// const bcrypt = require("bcrypt")
+// // const bcrypt = require("bcrypt")
+// const bcrypt = require("bcryptjs");
+
 // const jwt = require("jsonwebtoken")
 
 // const register = async (req, res) => {
@@ -101,8 +241,23 @@
 //             role: user.role,
 //         }
 //         const token = jwt.sign({ _id: user._id, emailId: emailId, role: role }, "wsac", { expiresIn: 60 * 60 })
-//         res.cookie('token', token, { maxAge: 60 * 60 * 1000 })
-        
+//         // res.cookie('token', token, { maxAge: 60 * 60 * 1000 })
+// //         res.cookie("token", token, {
+// //     httpOnly: true,
+// //     secure: true,      // REQUIRED for Render + Vercel
+// //     sameSite: "none",  // REQUIRED for cross-origin
+// //     maxAge: 60 * 60 * 1000
+// // });
+//             res.cookie("token", token, {
+//   httpOnly: true,
+//   secure: true,
+//   sameSite: "none",
+//   path: "/",                    // 🔥 REQUIRED
+//   domain: "swatesting-1.onrender.com", // 🔥 REQUIRED
+//   maxAge: 60 * 60 * 1000
+// });
+
+
 //         res.status(201).json({
 //             user: reply,
 //             message: 'user created successfully'
@@ -135,161 +290,188 @@
 //     }
 // }
 
+
 // module.exports = { register, login, logout, adminRegister, deleteprofile, getAllMember }
-const redisClient = require("../config/redis")
-const User = require("../model/user")
-const validate = require("../utlis/valitator")
-// const bcrypt = require("bcrypt")
+
+
+const redisClient = require("../config/redis");
+const User = require("../model/user");
+const validate = require("../utlis/valitator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const jwt = require("jsonwebtoken")
-
-const register = async (req, res) => {
-    try {
-        validate(req.body)
-        const { firstName, emailId, password, mobileNo } = req.body;
-        req.body.password = await bcrypt.hash(password, 10)
-        req.body.role = 'user'
-
-        const user = await User.create(req.body)
-
-        const reply = {
-            firstName: user.firstName,
-            emailId: user.emailId,
-            _id: user._id,
-            role: user.role,
-        }
-
-        const token = jwt.sign({ _id: user._id, emailId: emailId, role: "user" }, "wsac", { expiresIn: 60 * 60 })
-        res.cookie('token', token, { maxAge: 60 * 60 * 1000 })
-
-
-        res.status(201).json({
-            user: reply,
-            message: 'user created successfully'
-        })
-
-
-    } catch (err) {
-        res.status(400).send('ERROR: ' + err)
-    }
-}
-
-const login = async (req, res) => {
-    try {
-        const { emailId, password } = req.body
-        if (!emailId) {
-            throw new Error("Invalid Credentials")
-        }
-        if (!password) {
-            throw new Error("Invalid Credentials")
-        }
-        const user = await User.findOne({ emailId })
-
-        if (!user) {
-            return res.status(401).send('Invalid Credentials');
-        }
-
-        const match = await bcrypt.compare(password, user.password)
-
-        if (!match) {
-            throw new Error('Invalid Credentials')
-        }
-
-        const reply = {
-            firstName: user.firstName,
-            emailId: user.emailId,
-            _id: user._id,
-            role: user.role
-        }
-
-        const token = jwt.sign({ _id: user._id, emailId: emailId, role: user.role }, "wsac", { expiresIn: 60 * 60 })
-        res.cookie('token', token, { maxAge: 60 * 60 * 1000 })
-
-        res.status(201).json({
-            user: reply,
-            message: "Logged In Successfully"
-        })
-    } catch (err) {
-        res.status(401).send('ERROR: ' + err)
-    }
-}
-
-const logout = async (req, res) => {
-    try {
-        const { token } = req.cookies
-        const payload = jwt.decode(token);
-        await redisClient.set(`token:${token}`, "Blocked")
-        await redisClient.expireAt(`token:${token}`, payload.exp)
-        res.cookie("token", null, { expires: new Date(Date.now()) })
-        res.status(200).send("Logged Out Succesfully")
-    } catch (err) {
-        res.status(503).send("ERROR: " + err)
-    }
-}
-
-const adminRegister = async (req, res) => {
-    try {
-        validate(req.body)
-        const { firstName, emailId, password, mobileNo, role } = req.body;
-        req.body.password = await bcrypt.hash(password, 10)
-        const user = await User.create(req.body)
-        const reply = {
-            firstName: user.firstName,
-            emailId: user.emailId,
-            _id: user._id,
-            role: user.role,
-        }
-        const token = jwt.sign({ _id: user._id, emailId: emailId, role: role }, "wsac", { expiresIn: 60 * 60 })
-        // res.cookie('token', token, { maxAge: 60 * 60 * 1000 })
-//         res.cookie("token", token, {
-//     httpOnly: true,
-//     secure: true,      // REQUIRED for Render + Vercel
-//     sameSite: "none",  // REQUIRED for cross-origin
-//     maxAge: 60 * 60 * 1000
-// });
-            res.cookie("token", token, {
+/* ================= COOKIE CONFIG ================= */
+const cookieOptions = {
   httpOnly: true,
-  secure: true,
-  sameSite: "none",
-  path: "/",                    // 🔥 REQUIRED
-  domain: "swatesting-1.onrender.com", // 🔥 REQUIRED
-  maxAge: 60 * 60 * 1000
-});
+  secure: true,        // REQUIRED on Render
+  sameSite: "None",    // REQUIRED for cross-origin
+  maxAge: 60 * 60 * 1000,
+};
 
+/* ================= REGISTER ================= */
+const register = async (req, res) => {
+  try {
+    validate(req.body);
 
-        res.status(201).json({
-            user: reply,
-            message: 'user created successfully'
-        })
-    } catch (err) {
-        res.status(400).send('ERROR: ' + err)
+    const { password } = req.body;
+    req.body.password = await bcrypt.hash(password, 10);
+    req.body.role = "user";
+
+    const user = await User.create(req.body);
+
+    const reply = {
+      firstName: user.firstName,
+      emailId: user.emailId,
+      _id: user._id,
+      role: user.role,
+    };
+
+    const token = jwt.sign(
+      { _id: user._id, emailId: user.emailId, role: "user" },
+      process.env.JWT_SECRET || "wsac",
+      { expiresIn: "1h" }
+    );
+
+    res.cookie("token", token, cookieOptions);
+
+    res.status(201).json({
+      user: reply,
+      message: "User created successfully",
+    });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+/* ================= LOGIN ================= */
+const login = async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    if (!emailId || !password) {
+      return res.status(401).json({ error: "Invalid Credentials" });
     }
-}
 
+    const user = await User.findOne({ emailId });
+    if (!user) {
+      return res.status(401).json({ error: "Invalid Credentials" });
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.status(401).json({ error: "Invalid Credentials" });
+    }
+
+    const reply = {
+      firstName: user.firstName,
+      emailId: user.emailId,
+      _id: user._id,
+      role: user.role,
+    };
+
+    const token = jwt.sign(
+      { _id: user._id, emailId: user.emailId, role: user.role },
+      process.env.JWT_SECRET || "wsac",
+      { expiresIn: "1h" }
+    );
+
+    res.cookie("token", token, cookieOptions);
+
+    res.status(200).json({
+      user: reply,
+      message: "Logged in successfully",
+    });
+  } catch (err) {
+    res.status(401).json({ error: err.message });
+  }
+};
+
+/* ================= LOGOUT ================= */
+const logout = async (req, res) => {
+  try {
+    const token = req.cookies?.token;
+    if (!token) return res.status(200).send("Already logged out");
+
+    const payload = jwt.decode(token);
+    if (payload) {
+      await redisClient.set(`token:${token}`, "Blocked");
+      await redisClient.expireAt(`token:${token}`, payload.exp);
+    }
+
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    });
+
+    res.status(200).send("Logged out successfully");
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/* ================= ADMIN REGISTER ================= */
+const adminRegister = async (req, res) => {
+  try {
+    validate(req.body);
+
+    const { password } = req.body;
+    req.body.password = await bcrypt.hash(password, 10);
+
+    const user = await User.create(req.body);
+
+    const reply = {
+      firstName: user.firstName,
+      emailId: user.emailId,
+      _id: user._id,
+      role: user.role,
+    };
+
+    const token = jwt.sign(
+      { _id: user._id, emailId: user.emailId, role: user.role },
+      process.env.JWT_SECRET || "wsac",
+      { expiresIn: "1h" }
+    );
+
+    res.cookie("token", token, cookieOptions);
+
+    res.status(201).json({
+      user: reply,
+      message: "Admin created successfully",
+    });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
+
+/* ================= DELETE PROFILE ================= */
 const deleteprofile = async (req, res) => {
-    try {
-        const userId = req.result._id;
-        await User.findByIdAndDelete(userId)
-        res.status(200).send("Deleted Successfully")
-    } catch (err) {
+  try {
+    const userId = req.result._id;
+    await User.findByIdAndDelete(userId);
+    res.status(200).send("Deleted Successfully");
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
+};
 
-        res.status(500).send("SERVER ERROR")
-    }
-}
-
+/* ================= GET ALL USERS ================= */
 const getAllMember = async (req, res) => {
-    console.log("admin is here");
+  try {
+    const users = await User.find({})
+      .select("_id firstName emailId mobileNo role purchage cart");
 
-    try {
-        const getuser = await User.find({}).select('_id firstName emailId mobileNo role purchage cart')
-        res.status(200).send(getuser);
-    } catch (err) {
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).send("Server Error");
+  }
+};
 
-        res.status(500).send("SERVER ERROR")
-    }
-}
-
-
-module.exports = { register, login, logout, adminRegister, deleteprofile, getAllMember }
-
+module.exports = {
+  register,
+  login,
+  logout,
+  adminRegister,
+  deleteprofile,
+  getAllMember,
+};
